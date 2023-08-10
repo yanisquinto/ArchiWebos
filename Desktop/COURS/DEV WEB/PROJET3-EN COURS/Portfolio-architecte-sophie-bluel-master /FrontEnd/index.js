b9f1906f70752e5fb781b49e1156bd30f1254585
yanisquinto <yanis.quinto@mail.ch>
2023-08-10 17:53:15 +0800

    filtres des travaux (crées via JS) fonctionnels

--- Desktop/COURS/DEV WEB/PROJET3-EN COURS/Portfolio-architecte-sophie-bluel-master /FrontEnd/index.js
+++ Desktop/COURS/DEV WEB/PROJET3-EN COURS/Portfolio-architecte-sophie-bluel-master /FrontEnd/index.js
@@ -1,9 +1,10 @@
+// Attend que le DOM soit entièrement chargé avant d'exécuter le code
 document.addEventListener('DOMContentLoaded', function() {
-    // Sélection des éléments HTML nécessaires
-    const gallerySection = document.getElementById('gallery');
-    const filtersContainer = document.getElementById('filters');
+    // Sélectionne les éléments HTML nécessaires
+    const gallerySection = document.getElementById('gallery'); // Section où les images seront affichées
+    const filtersContainer = document.getElementById('filters'); // Conteneur des filtres
 
-    // Définition des catégories de filtres
+    // Définition des catégories de filtres avec leurs IDs et noms
     const categories = [
         { id: -1, name: 'Tous' },
         { id: 1, name: 'Objets' },
@@ -11,28 +12,36 @@ document.addEventListener('DOMContentLoaded', function() {
         { id: 3, name: 'Hôtels & restaurants' }
     ];
 
+    // Création des boutons de filtres en utilisant les catégories définies
+    categories.forEach(category => {
+        const button = document.createElement('button'); // Crée un élément bouton
+        button.textContent = category.name; // Définit le texte du bouton avec le nom de la catégorie
+        button.setAttribute('data-category', category.id); // Attribut personnalisé pour stocker l'ID de catégorie
+        filtersContainer.appendChild(button); // Ajoute le bouton au conteneur des filtres
+    });
+
     // Fonction pour filtrer et afficher les images en fonction de la catégorie sélectionnée
     function filterAndDisplayImages(categoryId, data) {
-        gallerySection.innerHTML = ''; // Effacer le contenu actuel de la galerie
+        gallerySection.innerHTML = ''; // Efface le contenu actuel de la galerie
 
-        // Filtrer les images en fonction de la catégorie sélectionnée
+        // Filtre les images en fonction de la catégorie sélectionnée
         const filteredImages = data.filter(image => categoryId === -1 || image.categoryId === categoryId);
 
-        // Parcourir les images filtrées et les afficher
+        // Parcours les images filtrées et les affiche
         filteredImages.forEach(image => {
-            const figure = document.createElement('figure');
-            const img = document.createElement('img');
-            const figcaption = document.createElement('figcaption');
+            const figure = document.createElement('figure'); // Crée un élément de figure
+            const img = document.createElement('img'); // Crée un élément d'image
+            const figcaption = document.createElement('figcaption'); // Crée un élément de légende
 
-            img.src = image.imageUrl;
-            img.alt = image.title;
+            img.src = image.imageUrl; // Définit l'URL de la source de l'image
+            img.alt = image.title; // Définit le texte alternatif de l'image
 
-            figcaption.textContent = image.title;
+            figcaption.textContent = image.title; // Définit le texte de la légende
 
-            figure.appendChild(img);
-            figure.appendChild(figcaption);
+            figure.appendChild(img); // Ajoute l'image à la figure
+            figure.appendChild(figcaption); // Ajoute la légende à la figure
 
-            gallerySection.appendChild(figure);
+            gallerySection.appendChild(figure); // Ajoute la figure à la section de la galerie
         });
     }
 
@@ -45,29 +54,23 @@ document.addEventListener('DOMContentLoaded', function() {
         // Traitement des données récupérées
         console.log(data);
 
-        // Création des boutons de filtres
+        filterAndDisplayImages(-1, data); // Affiche toutes les images par défaut
+
+        // Sélectionne tous les boutons de filtres
         const filterButtons = filtersContainer.querySelectorAll('button');
+        
+        // Ajoute un gestionnaire d'événement pour chaque bouton de filtre
         filterButtons.forEach(button => {
-            // Ajout d'un gestionnaire d'événement pour chaque bouton de filtre
             button.addEventListener('click', function(event) {
                 event.preventDefault(); // Empêche le rechargement par défaut de la page
                 const categoryId = parseInt(button.getAttribute('data-category'));
                 filterAndDisplayImages(categoryId, data); // Filtrer et afficher les images
             });
         });
-
-        // Création des boutons de filtres à partir des catégories définies
-        categories.forEach(category => {
-            const button = document.createElement('button');
-            button.textContent = category.name;
-            button.setAttribute('data-category', category.id);
-            filtersContainer.appendChild(button); // Ajouter le bouton au conteneur des filtres
-        });
-
-        filterAndDisplayImages(-1, data); // Afficher toutes les images par défaut
     })
     .catch(error => {
         // Gestion des erreurs
         console.error('Une erreur s\'est produite:', error);
     });
 });
+
