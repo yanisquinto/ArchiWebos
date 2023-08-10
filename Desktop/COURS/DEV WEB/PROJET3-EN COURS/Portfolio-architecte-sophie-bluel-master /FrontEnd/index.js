aefa1d69c10cd165d697eb94a5e7f4d7a05581f6
yanisquinto <yanis.quinto@mail.ch>
2023-08-10 17:17:24 +0800

    ajout des filtres via JS (non fonctionnels)

--- Desktop/COURS/DEV WEB/PROJET3-EN COURS/Portfolio-architecte-sophie-bluel-master /FrontEnd/index.js
+++ Desktop/COURS/DEV WEB/PROJET3-EN COURS/Portfolio-architecte-sophie-bluel-master /FrontEnd/index.js
@@ -1,6 +1,42 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-    // APPEL DE L'API AVEC FETCH
+    // Sélection des éléments HTML nécessaires
+    const gallerySection = document.getElementById('gallery');
+    const filtersContainer = document.getElementById('filters');
+
+    // Définition des catégories de filtres
+    const categories = [
+        { id: -1, name: 'Tous' },
+        { id: 1, name: 'Objets' },
+        { id: 2, name: 'Appartements' },
+        { id: 3, name: 'Hôtels & restaurants' }
+    ];
+
+    // Fonction pour filtrer et afficher les images en fonction de la catégorie sélectionnée
+    function filterAndDisplayImages(categoryId, data) {
+        gallerySection.innerHTML = ''; // Effacer le contenu actuel de la galerie
+
+        // Filtrer les images en fonction de la catégorie sélectionnée
+        const filteredImages = data.filter(image => categoryId === -1 || image.categoryId === categoryId);
+
+        // Parcourir les images filtrées et les afficher
+        filteredImages.forEach(image => {
+            const figure = document.createElement('figure');
+            const img = document.createElement('img');
+            const figcaption = document.createElement('figcaption');
+
+            img.src = image.imageUrl;
+            img.alt = image.title;
+
+            figcaption.textContent = image.title;
+
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+
+            gallerySection.appendChild(figure);
+        });
+    }
+
+    // Appel à l'API pour récupérer les données des travaux de la galerie
     fetch('http://localhost:5678/api/works', {
         method: 'GET'
     })
@@ -9,136 +45,29 @@ document.addEventListener('DOMContentLoaded', function() {
         // Traitement des données récupérées
         console.log(data);
 
-        const gallerySection = document.getElementById('gallery');
-
-        data.forEach(image => {
-            const figure = document.createElement('figure');
-            const img = document.createElement('img');
-            const figcaption = document.createElement('figcaption');
-          
-            img.src = image.imageUrl;
-            img.alt = image.title;
-          
-            figcaption.textContent = image.title;
-          
-            figure.appendChild(img);
-            figure.appendChild(figcaption);
-          
-            gallerySection.appendChild(figure);
+        // Création des boutons de filtres
+        const filterButtons = filtersContainer.querySelectorAll('button');
+        filterButtons.forEach(button => {
+            // Ajout d'un gestionnaire d'événement pour chaque bouton de filtre
+            button.addEventListener('click', function(event) {
+                event.preventDefault(); // Empêche le rechargement par défaut de la page
+                const categoryId = parseInt(button.getAttribute('data-category'));
+                filterAndDisplayImages(categoryId, data); // Filtrer et afficher les images
+            });
         });
+
+        // Création des boutons de filtres à partir des catégories définies
+        categories.forEach(category => {
+            const button = document.createElement('button');
+            button.textContent = category.name;
+            button.setAttribute('data-category', category.id);
+            filtersContainer.appendChild(button); // Ajouter le bouton au conteneur des filtres
+        });
+
+        filterAndDisplayImages(-1, data); // Afficher toutes les images par défaut
     })
     .catch(error => {
         // Gestion des erreurs
         console.error('Une erreur s\'est produite:', error);
     });
 });
-
-
-
-
-// CREATELEMENT POUR EXTRAIRE LES DONNEES DE L'API//
-
-const imageData = [
-    {
-      id: 1,
-      title: 'Abajour Tahina',
-      imageUrl: 'http://localhost:5678/images/abajour-tahina1651286843956.png',
-      categoryId: 1,
-      userId: 1,
-      createdAt: '2022-08-17T08:34:03.959Z',
-      updatedAt: '2022-08-17T08:34:03.959Z'
-    },
-    {
-      id: 2,
-      title: 'Appartement Paris V',
-      imageUrl: 'http://localhost:5678/images/appartement-paris-v1651287270508.png',
-      categoryId: 2,
-      userId: 1,
-      createdAt: '2022-08-17T08:34:30.508Z',
-      updatedAt: '2022-08-17T08:34:30.508Z'
-    },
-    {
-      id: 3,
-      title: 'Restaurant Sushisen - Londres',
-      imageUrl: 'http://localhost:5678/images/restaurant-sushisen-londres1651287319271.png',
-      categoryId: 3,
-      userId: 1,
-      createdAt: '2022-08-17T08:34:50.271Z',
-      updatedAt: '2022-08-17T08:34:50.271Z'
-    },
-    {
-      id: 4,
-      title: 'Villa “La Balisiere” - Port Louis',
-      imageUrl: 'http://localhost:5678/images/la-balisiere1651287350102.png',
-      categoryId: 2,
-      userId: 1,
-      createdAt: '2022-08-17T08:35:00.102Z',
-      updatedAt: '2022-08-17T08:35:00.102Z'
-    },
-    {
-      id: 5,
-      title: 'Structures Thermopolis',
-      imageUrl: 'http://localhost:5678/images/structures-thermopolis1651287380258.png',
-      categoryId: 1,
-      userId: 1,
-      createdAt: '2022-08-17T08:35:20.258Z',
-      updatedAt: '2022-08-17T08:35:20.258Z'
-    },
-    {
-      id: 6,
-      title: 'Appartement Paris X',
-      imageUrl: 'http://localhost:5678/images/appartement-paris-x1651287435459.png',
-      categoryId: 2,
-      userId: 1,
-      createdAt: '2022-08-17T08:35:35.459Z',
-      updatedAt: '2022-08-17T08:35:35.459Z'
-    },
-    {
-      id: 7,
-      title: 'Pavillon “Le coteau” - Cassis',
-      imageUrl: 'http://localhost:5678/images/le-coteau-cassis1651287469876.png',
-      categoryId: 2,
-      userId: 1,
-      createdAt: '2022-08-17T08:35:49.876Z',
-      updatedAt: '2022-08-17T08:35:49.876Z'
-    },
-    {
-      id: 8,
-      title: 'Villa Ferneze - Isola d’Elba',
-      imageUrl: 'http://localhost:5678/images/villa-ferneze1651287511604.png',
-      categoryId: 2,
-      userId: 1,
-      createdAt: '2022-08-17T08:36:03.755Z',
-      updatedAt: '2022-08-17T08:36:03.755Z'
-    },
-    {
-      id: 9,
-      title: 'Appartement Paris XVIII',
-      imageUrl: 'http://localhost:5678/images/appartement-paris-xviii1651287541053.png',
-      categoryId: 2,
-      userId: 1,
-      createdAt: '2022-08-17T08:36:13.119Z',
-      updatedAt: '2022-08-17T08:36:13.119Z'
-    },
-    {
-      id: 10,
-      title: 'Bar “Lullaby” - Paris',
-      imageUrl: 'http://localhost:5678/images/bar-lullaby-paris1651287567130.png',
-      categoryId: 3,
-      userId: 1,
-      createdAt: '2022-08-17T08:36:27.240Z',
-      updatedAt: '2022-08-17T08:36:27.240Z'
-    },
-    {
-      id: 11,
-      title: 'Hotel First Arte - New Delhi',
-      imageUrl: 'http://localhost:5678/images/hotel-first-arte-new-delhi1651287605585.png',
-      categoryId: 3,
-      userId: 1,
-      createdAt: '2022-08-17T08:36:45.585Z',
-      updatedAt: '2022-08-17T08:36:45.585Z'
-    }
-  ];
-
-  
-
