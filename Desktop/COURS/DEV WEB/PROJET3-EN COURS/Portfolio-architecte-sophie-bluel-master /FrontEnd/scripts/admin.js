ee82ed4a930438791054cfb079db7f18c5dba628
yanisquinto <yanis.quinto@mail.ch>
2023-09-02 13:10:22 +0200

    Projet terminé derniere MAJ avant ajustement pour soutenance

--- Desktop/COURS/DEV WEB/PROJET3-EN COURS/Portfolio-architecte-sophie-bluel-master /FrontEnd/scripts/admin.js
+++ Desktop/COURS/DEV WEB/PROJET3-EN COURS/Portfolio-architecte-sophie-bluel-master /FrontEnd/scripts/admin.js
@@ -53,21 +53,114 @@ document.addEventListener('DOMContentLoaded', function() {
         // Ajouter le bouton "Modifier" au début de la section Portfolio
         portfolioSection.insertBefore(portfolioEditButton, portfolioSection.firstElementChild);
 
-        // Créer la modal en dehors de la condition
+        // Création de la modale
         const modal = document.createElement('div');
         modal.id = 'myModal'; // Ajout de l'ID "myModal" à la modale
         modal.className = 'modal';
-        
+
         const modalContent = document.createElement('div');
         modalContent.className = 'modal-content';
         modalContent.innerHTML = `
             <span class="close" id="close">&times;</span>
-            <p>Contenu de la modale...</p>
+            <h1 class="modal-title">Galerie</h1>
         `;
+
+        // Ajout du bouton "Ajouter une image"
+        const addButton = document.createElement('button');
+        addButton.textContent = 'Ajouter une image';
+        addButton.addEventListener('click', () => {
+            const form = document.createElement('form');
+
+            // Récupérez les catégories depuis l'API
+            fetch('http://localhost:5678/api/categories', {
+                method: 'GET',
+            })
+            .then(response => response.json())
+            .then(categories => {
+                // Créez une liste déroulante (select) pour les catégories
+                const categorySelect = document.createElement('select');
+                categorySelect.id = 'category';
+                categorySelect.name = 'category';
+
+                // Remplissez la liste déroulante avec les catégories depuis l'API
+                categories.forEach(category => {
+                    const option = document.createElement('option');
+                    option.value = category.id;
+                    option.textContent = category.name;
+                    categorySelect.appendChild(option);
+                });
+
+                // Ajoutez les champs au formulaire
+                form.innerHTML = `
+                    <label for="image">Image :</label>
+                    <input type="file" id="image" name="image" accept="image/*" required>
+                    <br>
+                    <label for="title">Titre :</label>
+                    <input type="text" id="title" name="title" required>
+                    <br>
+                    <label for="category">Catégorie :</label>
+                `;
+
+                // Ajoutez la liste déroulante des catégories au formulaire
+                form.appendChild(categorySelect);
+
+                // Ajoutez le reste du formulaire
+                form.innerHTML += `
+                    <br>
+                    <button type="submit">Envoyer</button>
+                `;
+
+                // Écoutez l'événement de soumission du formulaire
+                form.addEventListener('submit', (event) => {
+                    event.preventDefault(); // Empêchez la soumission par défaut du formulaire
+                    const formData = new FormData(form);
+
+                    // Appelez la fonction pour téléverser l'image
+                    uploadImage(formData);
+                });
+
+                modalContent.appendChild(form);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des catégories :', error);
+            });
+        });
+
+        // Fonction pour téléverser l'image en requête POST
+        function uploadImage(formData) {
+            // Récupérez le token d'authentification depuis le stockage local
+            const token = localStorage.getItem('token');
+
+            // Vérifiez si le token existe
+            if (!token) {
+                console.error('Token d\'authentification manquant.');
+                return;
+            }
+
+            // Définissez l'en-tête Authorization avec le token
+            const headers = new Headers();
+            headers.append('Authorization', `Bearer ${token}`);
+
+            fetch('http://localhost:5678/api/works', {
+                method: 'POST',
+                body: formData,
+                headers: headers, // Ajoutez l'en-tête Authorization
+            })
+            .then(response => response.json())
+            .then(data => {
+                refreshModalContent();
+            })
+            .catch(error => {
+                console.error('Erreur lors du téléversement de l\'image :', error);
+            });
+        }
+
+        modalContent.appendChild(addButton);
 
         modal.appendChild(modalContent);
         document.body.appendChild(modal);
 
+        // Rafraîchissez le contenu de la modale lorsque la modale s'ouvre
         const openModalButton = document.getElementById('portfolio-edit-button');
         const closeModal = document.getElementById('close');
         const overlay = document.createElement('div');
@@ -78,6 +171,7 @@ document.addEventListener('DOMContentLoaded', function() {
         openModalButton.addEventListener('click', () => {
             modal.style.display = 'block';
             overlay.style.display = 'block'; // Afficher l'overlay
+            refreshModalContent(); // Rafraîchissez le contenu de la modale
         });
 
         closeModal.addEventListener('click', () => {
@@ -88,18 +182,74 @@ document.addEventListener('DOMContentLoaded', function() {
         overlay.addEventListener('click', (event) => {
             if (event.target === overlay) {
                 modal.style.display = 'none';
-                overlay.style.display = 'none'; // Masquer l'overlay si on clique à l'extérieur de la modale
+                overlay.style.display = 'none'; // Fermer la modale si on clique ailleurs sur la page
             }
         });
+
+        // Création une fonction pour mettre à jour le contenu de la modale
+        function refreshModalContent() {
+            //requête GET à l'API pour récupérer les données de la galerie
+            fetch('http://localhost:5678/api/works', {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(data => {
+                const modalContent = document.querySelector('.modal-content');
+
+                // Nettoyez le contenu existant de la modale
+                modalContent.innerHTML = `
+                    <span class="close" id="close">&times;</span>
+                    <h1 class="modal-title">Galerie</h1>
+                `;
+
+                // Ajouter chaque image avec le bouton "Supprimer"
+                data.forEach(imageData => {
+                    const imageElement = document.createElement('img');
+                    imageElement.src = imageData.imageUrl;
+                    imageElement.classList.add('modal-image');
+                    modalContent.appendChild(imageElement);
+
+                    const deleteButton = document.createElement('button');
+                    deleteButton.classList.add('icon-button'); // Ajoutez la classe personnalisée
+                    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can" style="color: #ffffff;"></i>'; // Ajoutez l'icône FontAwesome
+                    deleteButton.addEventListener('click', () => {
+                        const confirmDelete = confirm('Voulez-vous vraiment supprimer cette image ?');
+                        if (confirmDelete) {
+                            // Récupérez le token d'authentification depuis le stockage local
+                            const token = localStorage.getItem('token');
+
+                            // Vérifiez si le token existe
+                            if (!token) {
+                                console.error('Token d\'authentification manquant.');
+                                return;
+                            }
+
+                            // Définissez l'en-tête Authorization avec le token
+                            const headers = new Headers();
+                            headers.append('Authorization', `Bearer ${token}`);
+
+                            // Effectuez la requête DELETE avec l'en-tête d'authentification
+                            fetch(`http://localhost:5678/api/works/${imageData.id}`, {
+                                method: 'DELETE',
+                                headers: headers, // Ajoutez l'en-tête Authorization
+                            })
+                            .then(() => {
+                                // L'image a été supprimée avec succès, rafraîchissez la modale
+                                refreshModalContent();
+                            })
+                            .catch(error => {
+                                console.error('Erreur lors de la suppression de l\'image :', error);
+                            });
+                        }
+                    });
+
+                    modalContent.appendChild(deleteButton);
+                });
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des données de la galerie :', error);
+            });
+        }
     }
 });
-
-
-
-
-
-
-
-
-
 
